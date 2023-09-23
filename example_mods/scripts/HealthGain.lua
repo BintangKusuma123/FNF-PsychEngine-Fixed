0ea599ad664d20d814ef83dc1a368723072292b4
BintangKusuma123 <95070885+BintangKusuma123@users.noreply.github.com>
2023-09-23 23:45:16 +0700

    Delete example_mods/scripts/HealthGain.lua

--- example_mods/scripts/HealthGain.lua
+++
@@ -1,5 +0,0 @@
-function goodNoteHit(id, direction, noteType, isSustainNote)
-    --We add a x2 to byfriends current note hit health
-    health = getProperty('health')
-    setProperty('health', health+ 0.026);
-end
